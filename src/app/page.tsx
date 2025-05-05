8323b58c63f273353b2bdd8c83439b4ae5287afa
Leonardo Minora <leonardo.minora@gmail.com>
2025-05-05 16:10:31 -0300

    adicionado a atualização do dado compartilhado no componente filho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,27 @@ import React, { useState } from "react";
 
 const MeuBotao = (props) => {
 	const [contadorDoBotao, setContadorDoBotao] = useState(0);
+
+	const incrementar = (incremento) => {
+		setContadorDoBotao(contadorDoBotao + incremento);
+		props.funcao(incremento);
+	};
+
 	return (
 		<div className="bg-gray-100 p-6 rounded-lg shadow-sm border border-gray-200 max-w-xs mx-auto mb-4">
 			<button
 				className="bg-blue-600 hover:bg-blue-700 hover:cursor-pointer text-white py-2 px-4 text-base rounded-full"
-				onClick={() => setContadorDoBotao(contadorDoBotao + props.incremento)}
+				onClick={() => incrementar(props.incremento)}
 			>
 				{props.titulo}
 			</button>
 			<p className="text-sm">
-				O valor do <span className="font-bold">contador deste botão</span> é {contadorDoBotao}
+				O valor do <span className="font-bold">contador deste botão</span> é{" "}
+				{contadorDoBotao}
 			</p>
 			<p className="text-sm">
-				O valor do <span className="font-bold">contador compartilhado</span> é {props.contador}
+				O valor do <span className="font-bold">contador compartilhado</span> é{" "}
+				{props.contador}
 			</p>
 		</div>
 	);
@@ -31,11 +39,24 @@ const Home = () => {
 
 	return (
 		<div className="container mx-auto p-4 items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-			<h1 className="text-2xl font-bold mb-1 hover:cursor-pointer" onClick={() => contar(5)}>
+			<h1
+				className="text-2xl font-bold mb-1 hover:cursor-pointer"
+				onClick={() => contar(5)}
+			>
 				Valor do contador compartilhado é {contadorCompartilhado}
 			</h1>
-			<MeuBotao titulo="+1" incremento={1} contador={contadorCompartilhado} />
-			<MeuBotao titulo="+10" incremento={10} contador={contadorCompartilhado} />
+			<MeuBotao
+				titulo="+1"
+				incremento={1}
+				contador={contadorCompartilhado}
+				funcao={contar}
+			/>
+			<MeuBotao
+				titulo="+10"
+				incremento={10}
+				contador={contadorCompartilhado}
+				funcao={contar}
+			/>
 		</div>
 	);
 };
