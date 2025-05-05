b4b692e5a2c70d5b4d8422e2bb1235813a2a53c9
Leonardo Minora <leonardo.minora@gmail.com>
2025-05-05 15:54:29 -0300

    adicionado estado compartilhado e corrigido alguns estilos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,35 @@ const MeuBotao = (props) => {
 	return (
 		<div className="bg-gray-100 p-6 rounded-lg shadow-sm border border-gray-200 max-w-xs mx-auto mb-4">
 			<button
-				className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 text-base rounded-full"
+				className="bg-blue-600 hover:bg-blue-700 hover:cursor-pointer text-white py-2 px-4 text-base rounded-full"
 				onClick={() => setContadorDoBotao(contadorDoBotao + props.incremento)}
 			>
 				{props.titulo}
 			</button>
-			<p>O valor do contador deste botão é {contadorDoBotao}</p>
+			<p className="text-sm">
+				O valor do <span className="font-bold">contador deste botão</span> é {contadorDoBotao}
+			</p>
+			<p className="text-sm">
+				O valor do <span className="font-bold">contador compartilhado</span> é {props.contador}
+			</p>
 		</div>
 	);
 };
 
 const Home = () => {
+	const [contadorCompartilhado, setContadorCompartilhado] = useState(0);
+
+	const contar = (incremento) => {
+		setContadorCompartilhado(contadorCompartilhado + incremento);
+	};
+
 	return (
 		<div className="container mx-auto p-4 items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-			<h1 className="text-2xl font-bold mb-1">Valor do contador global</h1>
-			<MeuBotao titulo="+1" incremento={1} />
-			<MeuBotao titulo="+10" incremento={10} />
+			<h1 className="text-2xl font-bold mb-1 hover:cursor-pointer" onClick={() => contar(5)}>
+				Valor do contador compartilhado é {contadorCompartilhado}
+			</h1>
+			<MeuBotao titulo="+1" incremento={1} contador={contadorCompartilhado} />
+			<MeuBotao titulo="+10" incremento={10} contador={contadorCompartilhado} />
 		</div>
 	);
 };
